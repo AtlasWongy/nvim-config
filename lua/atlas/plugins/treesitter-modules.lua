4681e1387a46eaa57e995e19bd4e7304875d2bb6
AtlasWongy <atlasyjyj@gmail.com>
2026-04-26 11:53:51 +0800

    Making changes

--- lua/atlas/plugins/treesitter-modules.lua
+++ lua/atlas/plugins/treesitter-modules.lua
@@ -1,25 +1,25 @@
 return {
     "MeanderingProgrammer/treesitter-modules.nvim",
-    config = function()
-    require("treesitter-modules").setup({
-        ensure_installed = { "python", "c_sharp", "gdscript", "c", "lua", "vim", "vimdoc", "query" },
-        auto_installed = true,
-        highlight = {
-            enable = true
-        },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<Leader>ss",
-                node_incremental = "<Leader>si",
-                scope_incremental = "<Leader>sc",
-                node_decremental = "<Leadersd",
-            }
-        },
-        textobjects = {
-            select = {
-            },
-        },
-    })
-    end,
+    --config = function()
+    --    require("treesitter-modules").setup({
+    --        ensure_installed = { "python", "c_sharp", "gdscript", "c", "lua", "vim", "vimdoc", "query" },
+    --        auto_installed = true,
+    --        highlight = {
+    --            enable = true
+    --        },
+    --        incremental_selection = {
+    --            enable = true,
+    --            keymaps = {
+    --                init_selection = "<Leader>ss",
+    --                node_incremental = "<Leader>si",
+    --                scope_incremental = "<Leader>sc",
+    --                node_decremental = "<Leadersd",
+    --            }
+    --        },
+    --        textobjects = {
+    --            select = {
+    --            },
+    --        },
+    --    })
+    --end,
 }
